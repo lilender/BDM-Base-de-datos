b5c375514b1b32452643e7149da4f37d18c37f86
lilender <96185142+lilender@users.noreply.github.com>
2024-10-19 11:11:26 -0600

    Update run.sql

--- run.sql
+++ run.sql
@@ -7,7 +7,7 @@ SOURCE tr_after_insert_nivel.sql;
 SOURCE tr_before_delete_clase.sql;
 SOURCE tr_before_delete_nivel.sql;
 
-SOURCE sp_buy_cursos.sql;
+-- SOURCE sp_buy_cursos.sql;
 SOURCE sp_update_aprendizajes.sql;
 SOURCE sp_update_categoria.sql;
 SOURCE sp_update_clases.sql;
